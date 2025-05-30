03cd6d72a5e2d136676c43954f328f3c627e2c50
DoanLy5120 <doanthaoly.hkqn@gmail.com>
2025-05-30 09:43:56 +0700

    header, footer và responsive footer

--- fe/lylyshop/src/pages/user/theme/footer/index.js
+++ fe/lylyshop/src/pages/user/theme/footer/index.js
@@ -1,9 +1,78 @@
-import { memo } from 'react';
+import { memo } from "react";
+import "./style.scss";
+import { Link } from "react-router-dom";
+import { IoLogoFacebook } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+
 
 function Footer() {
-    return (
-        <h1>Footer</h1>
-    )
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <div className="footer__about">
+              <h1 className="footer__about_logo">LYLY SHOP</h1>
+              <ul>
+                <li>Địa chỉ: 213 Dương Đình Nghệ</li>
+                <li>Phone: 0977-232-323</li>
+                <li>Email: lylyshop@gmail.com</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div className="footer__widget">
+              <h4>Cửa hàng</h4>
+              <ul>
+                <li>
+                  <Link to="">Liên hệ</Link>
+                </li>
+                <li>
+                  <Link to="">Thông tin về chúng tôi</Link>
+                </li>
+                <li>
+                  <Link to="">Sản phẩm kinh doanh</Link>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <Link to="">Thông tin tài khoản</Link>
+                </li>
+                <li>
+                  <Link to="">Giỏ hàng</Link>
+                </li>
+                <li>
+                  <Link to="">Danh sách ưa thích</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+            <div className="footer__widget">
+              <h4>Khuyến mãi & Ưu đãi</h4>
+              <p>Đăng ký nhận thông tin tại đây</p>
+              <form action="#">
+                <div className="input">
+                  <input type="text" placeholder="Nhập email"/>
+                  <button type="submit" className="button-submit">
+                    Đăng ký
+                  </button>
+                </div>
+                <div className="footer__widget_social">
+                  <div>
+                    <IoLogoFacebook />
+                  </div>
+                  <div>
+                    <FaInstagram />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default memo(Footer);
--- fe/lylyshop/src/pages/user/theme/header/index.js
+++ fe/lylyshop/src/pages/user/theme/header/index.js
@@ -1,9 +1,99 @@
-import { memo } from 'react';
+import { memo, useState } from "react";
+import "./style.scss";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { ROUTERS } from "utils/router";
 
 function Header() {
-    return (
-        <h1>Header</h1>
-    )
+  const [menus, setMenus] = useState([
+    {
+      name: "Trang chủ",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Cửa hàng",
+      path: ROUTERS.USER.PRODUCT,
+    },
+    {
+      name: "Sản phẩm",
+      path: "",
+      isShowSubMenu: false,
+      child: [
+        {
+          name: "Thịt",
+          path: "",
+        },
+        {
+          name: "Rau củ",
+          path: "",
+        },
+        {
+          name: "Thức ăn nhanh",
+          path: "",
+        },
+      ],
+    },
+    {
+      name: "Bài viết",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Liên hệ",
+      path: ROUTERS.USER.HOME,
+    },
+  ]);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-3">
+          <div className="header__logo">
+            <h1>LYLY SHOP</h1>
+          </div>
+        </div>
+        <div className="col-xl-6">
+          <nav className="header__menu">
+            <ul>
+              {menus?.map((menu, menuKey) => (
+                <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                  <Link to={menu?.path}>{menu?.name}</Link>
+                  {
+                    menu.child && (
+                        <ul className="header__menu_dropdown">
+                            {
+                                menu.child.map((childItem, childKey) => (
+                                    <li key={`${menuKey}-${childKey}`}>
+                                        <Link to={childItem.path}>{childItem.name}</Link>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    )
+                  }
+                </li>
+              ))}
+              
+            </ul>
+          </nav>
+        </div>
+        <div className="col-xl-3">
+          <div className="header__cart">
+            <div className="header__cart_price">
+              <span>100.123 đ</span>
+            </div>
+            <ul>
+              <li>
+                <Link to="#">
+                  <FaShoppingCart />
+                  <span>5</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default memo(Header);
